290367fa9e1ad272fbf2d75246d400a728ddf070
kyle-j13 <kylejohnston458@gmail.com>
2025-08-13 19:47:47 -0400

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 // ON PRODUCTION, CHANGE THE PROXY FOR THE WEBSITE
 export default defineConfig({
+  base: '/monty-hall/', // 
   plugins: [react()],
   server: {
     proxy: {
